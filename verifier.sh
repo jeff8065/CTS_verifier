eda8ce93d1d88e84bc428cfdb70b5a4051a19557
jeff8065 <37102860+jeff8065@users.noreply.github.com>
2020-12-22 18:06:07 +0800

    Update verifier.sh

--- verifier.sh
+++ verifier.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /CTS_tool/CTSV/3PL_verifier
 config=$(ls | grep config.ini)
-chmod 755 /CTS_tool/CTSV/3PL_verifier/'GMS Express Plus test script - Android R'/.*
+chmod 755 /CTS_tool/CTSV/3PL_verifier/'GMS Express Plus test script - Android R'/Express_20201023.sh
+chmod 755 /CTS_tool/CTSV/3PL_verifier/'GMS Express Plus test script - Android R'/get_coord.py
 function gitsync()
 {
 	cd /CTS_tool/CTSV/3PL_verifier
